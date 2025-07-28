a56d3fb0df4f9c3221f45e3b34a856db24abd7bd
Guilherme Taira <guilherme.taira@onr.org.br>
2025-07-28 00:15:01 -0300

    controlando visual com estado

---
+++ curso-1/cookin-up/src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default {
+  name: 'IngredienteSelecionavel',
+  components: {
+    Tag,
+  },
+  props: {
+    ingrediente: { type: String, required: true }
+  },
+  data() {
+    return {
+      selecionado: false,
+    }
+  }
+}
+</script>
+
+<template>
+  <button
+    class="ingrediente"
+    @click="selecionado = !selecionado"
+    :aria-pressed="selecionado"
+  >
+    <Tag :texto="ingrediente" :ativa="selecionado" />
+  </button>
+</template>
+
+<style scoped>
+.ingrediente {
+  cursor: pointer;
+}
+</style>
